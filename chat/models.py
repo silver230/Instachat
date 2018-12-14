74d5f4e15dd4c4b08aafcf445a67f035fe89808e
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-14 15:14:21 +0300

    created a profile class for the user wikth the method to save and delete

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+ from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from tinymce.models import HTMLField
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    profimage =  models.ImageField(upload_to='images')
+    bio = HTMLField(max_length=500)
+
+    
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+    
+    def __str__(self):
+        return self.user.username
+
+ 
