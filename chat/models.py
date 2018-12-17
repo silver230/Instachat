963e1ed1fa1aac74c91366ad1973cc6fdd671309
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-17 18:49:54 +0300

    added fieldson the profile class

--- chat/models.py
+++ chat/models.py
@@ -1,21 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from tinymce.models import HTMLField
-# Create your models here.
-
+# Create your models here
 
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
-    profimage =  models.ImageField(upload_to='media')
-    bio = HTMLField(max_length=500)
-    
-    def save_profile(self):
-        self.save()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    profimage = models.ImageField(upload_to='posts')
+    birth_date = models.DateField(null=True, blank=True)
 
-    def delete_profile(self):
-        self.delete()
-    
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+            instance.profile.save()
+
     def __str__(self):
         return self.bio
 
