9b651b49f4572e2632f36a31c419c5337b4dc996
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-17 15:07:39 +0300

    fined the paths of my url

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,25 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.index,name='index'),
+    url(r'^$',views.index,name="index"),
+    url(r'^search',views.search_results,name='search_results'),
+    url(r'^post/(\d+)', views.post, name='post'),
+    url(r'^new/post$', views.new_post, name='new-post'),
+    url(r'auth/',include('registration.backends.simple.urls')),
+    url(r'^sign/$',views.signup, name='signup'),
     # url(r'^profile/$',views.profile, name='profile'),
-    # url(r'^prof/(\d+)',views.profiles,name='prof'),
-]
+    # url(r'^prof/$',views.prof,name='prof'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+    # url(r'^follow/(?P<operation>.+)/(?P<id>\d+)',views.follow,name='follow'),
+    url(r'^comment/(\d+)',views.comment,name='comment'),
+    # url(r'^like/(\d+)',views.likes,name='likes'),
+    
+    
+    
+]    
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
