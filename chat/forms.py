8319bf094769059bc25ccb49e1b57f548ef46e07
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-17 15:02:02 +0300

    created newpost form signupform ,comments an dlikes to the parameters they needed

--- chat/forms.py
+++ chat/forms.py
@@ -1,8 +1,8 @@
-from django.db import models
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Posts,Comments,Profile,Likes
+
 
 
 class SignUpForm(UserCreationForm):
@@ -12,13 +12,36 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name','username', 'email', 'password1', 'password2', ]
+        fields = ['first_name', 'last_name','email', 'password1', 'password2', ]
 
-
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Posts
+        exclude = ['user','postedon']
+        fields = ['caption','image']
+       
 class Prof(forms.ModelForm):
     class Meta:
         model=Profile
         exclude=[]
         fields=['profimage','bio']
 
+
+class Comments(forms.ModelForm):
+    class Meta:
+        model=Comments
+        exclude=[]
+        fields=['comment']
+
+class Likes(forms.ModelForm):
+    class Meta:
+        model=Likes
+        exclude=[]
+        fields=[]
+
+# class NewPostForm(forms.ModelForm):
+#     class Meta:
+#         model =  Posts
+#         exclude = ['pub_date']
+#         fields =[]
  
