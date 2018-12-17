8258af14deca48ddffdefcdcab7f6c25ac3c0f5c
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-17 15:08:48 +0300

    activated  my signup file and create post and a search function for my code

--- chat/views.py
+++ chat/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render,redirect
 from . forms import SignUpForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from .models import Posts,Comments,Likes
+from .models import Posts,Profile,Follow
+from .forms import NewPostForm,Prof,Comments,Likes
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+
+
+
 
 # Create your views here
-# @login_required(login_url='/auth/login')
+# @login_required(login_url='/accounts/login/')
 def index(request):
     post = Posts.objects.all()
     comm = Comments()
@@ -43,4 +55,78 @@ def signup(request):
             return redirect('index')
     else:
         form = SignUpForm()
-    return render(request, 'singup_form.html', {'form': form})
+    return render(request, 'registration_form.html', {'form': form})
+
+
+ 
+
+ 
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.<a href="/auth/login">Login</a>')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+# @login_required(login_url='/accounts/login/')
+def post(request,post_id):
+    try:
+        post = Posts.objects.get(id = post_id)
+    except Posts.DoesNotExist:
+        raise Http404()
+    return render(request,"all-chats/post.html", {"post":post})
+
+# @login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.editor = current_user
+            post.save()
+        return redirect('index')
+
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form": form})  
+
+ 
+ 
+
+    return render(request, 'profile/edit_profile.html', {'form':form})
+@login_required(login_url='/auth/login')
+def comment(request,id):
+    post = Posts.objects.get(id=id)
+    if request.method == 'POST':
+        comm=Comments(request.POST)
+        if comm.is_valid():
+            comment=comm.save(commit=False)
+            comment.user = request.user
+            comment.post=post
+            comment.save()
+            return redirect('index')
+    return redirect('index')
+
+def search_results(request):
+    
+    if 'user' in request.GET and request.GET["user"]:
+        search_item = request.GET.get("user")
+        searched_users = User.objects.filter(username=search_item)
+        print(searched_users)
+        message = f"{search_item}"
+        return render(request, 'search.html',{"message":message,"users": searched_users})
+    else:
+        message = "You haven't searched for any user"
+        return render(request, 'search.html',{"message":message})
+
+
+     
