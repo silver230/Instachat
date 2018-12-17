5683d2f995fa2158b12ba525b826ac50113faee6
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-17 15:00:06 +0300

    imported posts and profile and set post to admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Posts,Profile
 # Register your models here.
+
+
+class PostsAdmin(admin.ModelAdmin):
+    filter_horizontal =('user',)
+
+admin.site.register(Posts,PostsAdmin)
+admin.site.register(Profile)
+ 
